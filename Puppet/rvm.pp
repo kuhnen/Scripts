ada55f9377a12776aabfea0d8aacdb00bc093f20
kuhnen <andrekuhnen@gmail.com>
2013-06-01 16:33:43 -0300

    added monitor configuration

--- Puppet/rvm.pp
+++ Puppet/rvm.pp
@@ -21,4 +21,28 @@ package { ["curl", 'scala', 'build-essential']:
   ensure => latest,
 }
 
+file { ['/home/kuhnen/Apps', '/home/kuhnen/bin']:
+  ensure => directory,
+
+}
+
+$monitor = $is_virtual ? {
+  'true' => ['VBOX1', 'VBOX2'],
+  default =>['LVDS', 'VGA-0'],
+}
+
+file{'/home/kuhnen/monitor_conf.sh':
+  ensure => file,
+  content =>
+
+  "#!/bin/bash
+  xrandr --output #{monitor[0]} --primary
+  xrandr --output #{monitor[1]} --right-of #{monitor[0]}
+  xrandr --output #{monitor[1]} --auto
+  xrandr --output #{monitor[0]} --auto
+  "
+  owner => 'kuhnen',
+  mode => 755,
+  }
+
 rvm_for {['kuhnen']: }
